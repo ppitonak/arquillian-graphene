e9fb37ef6ef68307c3bca2fc62618bd2c96e0a9a
Lukas Fryc <lfryc@redhat.com>
2012-04-18 10:48:40 +0200

    added working form submission interception feature

--- graphene-webdriver-impl/src/main/javascript/formSubmissionInterception.js
+++ graphene-webdriver-impl/src/main/javascript/formSubmissionInterception.js
@@ -1,32 +1,63 @@
 /**
- * JBoss, Home of Professional Open Source
- * Copyright 2012, Red Hat, Inc. and individual contributors
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * JBoss, Home of Professional Open Source Copyright 2012, Red Hat, Inc. and individual contributors by the
+ * 
+ * @authors tag. See the copyright.txt in the distribution for a full listing of individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 var Graphene = Graphene || {};
 
 Graphene.formSubmissionInterception = (function() {
-    
-    
-    
+
+    var lastSubmittedForm = null;
+    var passThroughForm = null;
+    var boundHandler;
+
+    var submitHandler = function(e) {
+        var form = e.target;
+
+        // when the submission should be passed through
+        if (passThroughForm === form) {
+            passThroughForm = null;
+            return true;
+        } else {
+            lastSubmittedForm = form;
+            e.stopImmediatePropagation();
+            return false;
+        }
+    };
+
     // PUBLIC METHODS
     return {
-        
+        inject : function() {
+            if (!boundHandler) {
+                boundHandler = $.proxy(submitHandler, this);
+                $(document).bind('submit', boundHandler);
+            }
+        },
+
+        uninject : function() {
+            if (boundHandler) {
+                $(document).unbind('submit', boundHandler);
+                boundHandler = null;
+            }
+        },
+
+        getSubmittedForm : function() {
+            return lastSubmittedForm;
+        },
+
+        submitForm : function() {
+            passThroughForm = lastSubmittedForm;
+            lastSubmittedForm.submit();
+            lastSubmittedForm = null;
+        }
     };
 })();
--- graphene-webdriver-impl/src/test/javascript/formSubmissionInterception-test.js
+++ graphene-webdriver-impl/src/test/javascript/formSubmissionInterception-test.js
@@ -2,12 +2,79 @@ module("Form Submission Interception");
 
 (function() {
 
-    var intr = Graphene.formSubmissionInterception;
-    
+    var fsi = Graphene.formSubmissionInterception;
+
     // TESTS
-    
+
     test("accessor object not null", function() {
-        ok(intr);
+        ok(fsi);
     });
-    
+
+    test("form can be submitted", function() {
+        $()
+        var form = document.getElementById("form1");
+        var submitButton = $(document.getElementById("form1:submit"));
+        window.form1Submitted = false;
+
+        submitButton.click();
+        ok(window.form1Submitted);
+    });
+
+    test("form is prevented to being submit by test", function() {
+        // given
+        var form = document.getElementById("form1");
+        var submitButton = $(document.getElementById("form1:submit"));
+        window.form1Submitted = false;
+
+        var handler = function(e) {
+            ok(e.target === form);
+            return false;
+        };
+
+        $(document).bind('submit', handler);
+
+        submitButton.click();
+        ok(!window.form1Submitted);
+
+        $(document).unbind('submit', handler);
+    });
+
+    test("form submission can be intercepted", function() {
+        // given
+        var form = document.getElementById("form1");
+        var submitButton = $(document.getElementById("form1:submit"));
+        window.form1Submitted = false;
+
+        ok(!fsi.getSubmittedForm());
+
+        // when
+        fsi.inject()
+        submitButton.click();
+
+        // then
+        ok(!window.form1Submitted);
+        ok(fsi.getSubmittedForm() === form);
+
+        // finally
+        fsi.uninject();
+    });
+
+    test("when form submission is intercepted then it can be continued", function() {
+        // given
+        var form = document.getElementById("form1");
+        var submitButton = $(document.getElementById("form1:submit"));
+        window.form1Submitted = false;
+        fsi.inject()
+        submitButton.click();
+        ok(!window.form1Submitted);
+
+        // when
+        fsi.submitForm();
+        ok(window.form1Submitted);
+        ok(!fsi.getSubmittedForm());
+
+        // finally
+        fsi.uninject();
+    });
+
 })();
